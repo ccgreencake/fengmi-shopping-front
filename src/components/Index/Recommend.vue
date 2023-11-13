910576125f02893eca3288e88ce88cb98738f1cc
Liu Dongbin <dobi0108@icloud.com>
2023-11-13 16:24:44 +0800

    推荐商品数据获取成功,并展示成功（图片已补上）

--- src/components/Index/Recommend.vue
+++ src/components/Index/Recommend.vue
@@ -2,7 +2,8 @@
     <el-row type="flex" style="margin-top: 15px;">
        <el-col class="goods-show" :offset="3" :span="18" >
             <el-card :body-style="style" v-for="(product,index) in products" :key="index">
-                <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png" class="image">
+                <el-image :src="'http://localhost:8080/img/'+product.productImgList[0].url" style="height: 200px"></el-image>
+<!--                <img src="'http://localhost:8080/img/'+product.productImgList[0].url" class="image">-->
                 <div style="padding: 14px;">
                     <span>{{ product.productName }}</span>
                     <div class="bottom clearfix">
@@ -27,7 +28,7 @@ export default {
        getProduct(){
          let _this = this;
          getProductList().then(response => {
-             window.console.log(response);
+             // window.console.log(response.data);
              _this.products = response.data;
          })
        }
